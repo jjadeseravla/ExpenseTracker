7ae3988fb82cfb45bbfb16557dfc1437e534d0b4
jjadeseravla <jadealvares@hotmail.co.uk>
2020-05-29 19:04:14 +0100

    Added a controller that calls server.js and that talks to routes

---
+++ controllers/transactionController.js
@@ -0,0 +1,6 @@
+// @desc Get all transactions
+//@route GET /api/v1/transactions
+//@access public
+exports.getTransactions = (req, res, next) => {
+  res.send('GET transactions');
+}
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { getTransactions } = require('../controllers/transactionController');
 
-router.get('/', (req, res) => res.send('router'));
+router
+    .route('/')
+    .get(getTransactions);
 
 module.exports = router;
