32054b3e6ec6ca5f283194188a152c2b38cbae86
jjadeseravla <jadealvares@hotmail.co.uk>
2020-05-29 19:08:28 +0100

    routes (POST and DELETE) are set up, now need to bring a model to interact with DB

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -4,3 +4,17 @@
 exports.getTransactions = (req, res, next) => {
   res.send('GET transactions');
 }
+
+// @desc Add all transactions
+//@route POST/api/v1/transactions
+//@access public
+exports.addTransaction = (req, res, next) => {
+  res.send('ADD transactions');
+}
+
+// @desc Delete all transactions
+//@route DELETE /api/v1/transactions
+//@access public
+exports.deleteTransaction = (req, res, next) => {
+  res.send('DELETE transactions');
+}
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getTransactions } = require('../controllers/transactionController');
+const { getTransactions, addTransaction, deleteTransaction } = require('../controllers/transactionController');
 
 router
     .route('/')
-    .get(getTransactions);
+    .get(getTransactions)
+    .post(addTransaction);
+
+//delete needs an id
+router
+    .route('/:id')
+    .delete(deleteTransaction);
 
 module.exports = router;
