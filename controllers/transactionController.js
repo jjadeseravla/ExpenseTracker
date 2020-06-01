b9d4277d4c5204c9c795429535ced4f6d5de8af0
jjadeseravla <jadealvares@hotmail.co.uk>
2020-06-01 10:41:16 +0100

    [WIP] Adding my MongoDB Atlas(cloud) but 'Internal Server Error' on Postman
    Error: Could not connect to any servers in your MongoDB Atlas cluster. One common reason is that you're trying to access the database from an IP that isn't whitelisted. Make sure your current IP address is on your Atlas cluster's IP whitelist: https://docs.atlas.mongodb.com/security-whitelist/

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,20 +1,36 @@
+const TransactionModel = require('../models/TransactionModel');
+
 // @desc Get all transactions
 //@route GET /api/v1/transactions
 //@access public
-exports.getTransactions = (req, res, next) => {
-  res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.sendStatus(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    });
+  } catch (err) {
+
+    return res.sendStatus(500).json({
+      success: false,
+      error: 'Server error'
+    });
+  }
 }
 
 // @desc Add all transactions
 //@route POST/api/v1/transactions
 //@access public
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send('ADD transactions');
 }
 
 // @desc Delete all transactions
-//@route DELETE /api/v1/transactions
+//@route DELETE /api/v1/transactions/:id
 //@access public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send('DELETE transactions');
 }
